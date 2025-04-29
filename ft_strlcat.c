db819f09501fd33dc4881b808d7173e89e38cf63
ramune256 <shungo180620@outlook.jp>
2025-04-29 17:29:07 +0900

    done strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: shunwata <shunwata@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 19:45:28 by shunwata          #+#    #+#             */
-/*   Updated: 2025/04/29 17:17:46 by shunwata         ###   ########.fr       */
+/*   Updated: 2025/04/29 17:28:47 by shunwata         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	src_len;
 	size_t	dst_len;
-	size_t	max_len;
 	size_t	i;
 
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-	max_len = src_len + dst_len + 1;
-
+	i = 0;
+	if (dstsize <= dst_len)
+		return (src_len + dstsize);
+	while (src[i] != '\0' && dst_len + i + 1 < dstsize)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
